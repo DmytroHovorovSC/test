7c4e198062a01534f531717151af4008a208c7d8
DmytroHovorovSC <dmytro.hovorov@looksery.com>
2018-04-16 20:54:07 +0300

    project state as by 14.04

---
+++ js/canvas.js
@@ -0,0 +1,12 @@
+testMethod = function() {
+	alert("Hello! I am an alert box!!");
+}
+
+
+
+function drawCanvas() {
+	var canvas = document.getElementById('tutorial');
+	if (canvas.getContext) {
+		var ctx = canvas.getContext('2d');
+	}
+}
